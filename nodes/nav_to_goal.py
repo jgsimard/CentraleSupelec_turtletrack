6e914f409a6d41282da66f44332d762e6d4bd5f0
jg <jeangab23@gmail.com>
2017-11-08 09:25:41 -0500

    it compiles

--- nodes/nav_to_goal.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-import rospy
-from std_msgs.msg import String, Float64
-from geometry_msgs.msg import Twist
-
--- src/nav_to_goal.cpp
+++ src/nav_to_goal.cpp
@@ -3,8 +3,10 @@
 #include <tf/transform_listener.h>
 #include <time.h>
 #include <geometry_msgs/Pose2D.h>
-#include <transform_pose/pose.h>
+#include "tl_turtle_track/pose.h"
 #include <angles/angles.h>
+#include <cmath>
+#include <geometry_msgs/Twist.h>
 
 #define BASE_TF "turtle1"
 #define WORLD_TF "world"
@@ -27,6 +29,7 @@ bool transformPose2D(const geometry_msgs::Pose2D& pose_src,
   pose_src_stamped.pose.orientation = tf::createQuaternionMsgFromYaw(pose_src.theta);
 
   tf::Stamped<tf::Pose> tf_pose_src, tf_pose_dst;
+  
   // Build up a tf::Stamped<Pose> from a geometry_msgs::PoseStamped
   tf::poseStampedMsgToTF(pose_src_stamped, tf_pose_src);
   
@@ -48,6 +51,7 @@ bool transformPose2D(const geometry_msgs::Pose2D& pose_src,
   // move the yaw in [-pi, pi]
   yaw = angles::normalize_angle(yaw);
 
+
   // Transform the tf::Pose back into the Pose2D
   pose_dst.x = tf_pose_dst.getOrigin().x();
   pose_dst.y = tf_pose_dst.getOrigin().y();
@@ -57,7 +61,7 @@ bool transformPose2D(const geometry_msgs::Pose2D& pose_src,
 }
 
 
-bool transform_callback(transform_pose::pose::Request& request, transform_pose::pose::Response& response) {
+bool transform_callback(tl_turtle_track::pose::Request& request, tl_turtle_track::pose::Response& response) {
 
   geometry_msgs::Pose2D goal = request.pose;
   geometry_msgs::Pose2D target_pose;
@@ -65,17 +69,49 @@ bool transform_callback(transform_pose::pose::Request& request, transform_pose::
   // Let us transform the current goal from the world to the 
   // base frame
   transformPose2D(goal, WORLD_TF, response.pose, BASE_TF);
- 
+
   return true;
 }
 
+#define MAX_DIST 0.1
+#define MAX_ANGL 0.1
+
+bool moveTo(const geometry_msgs::Pose2D& target_pose, geometry_msgs::Twist& cmd_vel)
+{
+  if(std::abs(target_pose.x) > MAX_DIST){ // align with destination
+    cmd_vel.linear.x = 0.0;
+    cmd_vel.angluar.z = target_pose.x;
+  }
+  
+  else if (target_pose.y > MAX_DIST){ //go to destination
+    cmd_vel.linear.x = target_pose.y;
+    cmd_vel.angluar.z = 0.0;
+  }
+  
+  else if (std::abs(target.theta) > MAX_ANGL){
+      cmd_vel.linear.x = 0.0;
+      cmd_vel.angluar.z = target_pose.theta;
+    }
+  else{
+     cmd_vel.linear.x = 0.0;
+     cmd_vel.angluar.z = 0.0;
+     return true;
+  }
+
+  return false;
+}
 int main(int argc, char* argv[]) {
 
   ros::init(argc, argv, "transform_pose2D");  
   ros::NodeHandle nh;
 
   ros::ServiceServer service = nh.advertiseService("transform_pose2D", transform_callback);
+
+  ros::NodeHandle node1
+  ros::Publisher chatter_pub = node1.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+  
   ROS_INFO("Ready to transform poses from %s into %s", WORLD_TF, BASE_TF);
+  
   ros::spin();
 
   return 0;
