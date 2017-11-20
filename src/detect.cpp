949eb1c04b6a08e0aefa71b8ee7d41c0e6ddfbbb
Simard Jean-Gabriel <simard_jea@calcul03.smart.metz.supelec.fr>
2017-11-20 19:01:44 +0100

    detect works

--- src/detect.cpp
+++ src/detect.cpp
@@ -3,6 +3,7 @@
 #include "tl_turtle_track/PanTilt.h"
 #include "tl_turtle_track/PanTilts.h"
 #include "pantiltzoom.hpp"
+//#include "axis_camera/Axis.h" //"tl_turtle_track/Axis.h"
 
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
@@ -12,7 +13,7 @@
 #include <vector>
 #include <array>
 
-#define NB_COLORS 4
+#define NB_COLORS 2
 
 void change_pos(ros::Publisher& pub, float hpos, float vpos) {
   tl_turtle_track::Axis pos;
@@ -47,7 +48,7 @@ void change_zoom(ros::Publisher& pub, float zoom) {
   ros::Duration(4).sleep();
 }
 
-#define TOLERANCE 10
+#define TOLERANCE 5
 
 void img_callback(const sensor_msgs::ImageConstPtr& msg,
 		  ros::Publisher &pub,
@@ -55,13 +56,14 @@ void img_callback(const sensor_msgs::ImageConstPtr& msg,
 {
   cv_bridge::CvImageConstPtr bridge_input;
   try {
-    bridge_input = cv_bridge::toCvShare(msg,sensor_msgs::image_encodings::RGB8);
+    bridge_input = cv_bridge::toCvShare(msg,sensor_msgs::image_encodings::BGR8);
   }
   catch (cv::Exception& e) {
     std::ostringstream errstr;
     errstr << "cv_bridge exception caught: " << e.what();
     return;
   }
+  ROS_INFO("super a march");
   
   const cv::Mat& input  = bridge_input->image;
   cv::Mat        rgb   (input.rows, input.cols, CV_8UC3);
@@ -73,12 +75,14 @@ void img_callback(const sensor_msgs::ImageConstPtr& msg,
   unsigned char* in           = begin_input;
 
   // Efficient way to process each channel in each pixel,
-  while(in != end_input) *(out++) = 255 - *(in++);
+  //  while(in != end_input) *(out++) = 255 - *(in++);
+   while(in != end_input) *(out++) = *(in++);
 
+  //pub.publish(cv_bridge::CvImage(msg->header, "rgb8", output).toImageMsg());
   
-  std::array<cv::Scalar,NB_COLORS> colors = {{cv::Scalar(255,0,0), {0,255,0}, {0,0,255}, {255,255,0}}};
+  std::array<cv::Scalar,NB_COLORS> colors = {cv::Scalar(10,0,0), cv::Scalar(165,0,0)};//ORANGE, ROSE
    
-  cv::Mat hsv,detection, cleaned;
+  cv::Mat hsv, tresh, detection, cleaned;
 
   double u0 = input.rows/2, v0 = input.cols/2;
   
@@ -89,72 +93,87 @@ void img_callback(const sensor_msgs::ImageConstPtr& msg,
   std::vector<cv::Vec4i>               hierarchy;
 
   tl_turtle_track::PanTilts pantilts;
+  std::vector<tl_turtle_track::PanTilt> vec_pantilt;
 
   cv::cvtColor(rgb, hsv, CV_BGR2HSV);       // convert it to HSV
+  
+  for(auto& color : colors){
 
-  for(int i = 0; i < NB_COLORS; ++i){
+    cv::Scalar hsv_min(color[0] - TOLERANCE, 50, 50);
+    cv::Scalar hsv_max(color[0] + TOLERANCE, 255, 255);
 
-    cv::Scalar hsv_min(colors[i][0] - TOLERANCE - 1, colors[i][1] - TOLERANCE, 0);
-    cv::Scalar hsv_max(colors[i][0] + TOLERANCE - 1, colors[i][1] + TOLERANCE, 255);
+    cv::inRange(hsv,hsv_min,hsv_max, tresh); // select in-range pixels
 
-    cv::inRange(hsv,hsv_min,hsv_max,detection); // select in-range pixels
-
-    cv::erode (detection,detection,open_elem);  // opening...
+    //nettoyer img
+    cv::erode (tresh,detection,open_elem);  // opening...
     cv::dilate(detection,detection,open_elem);  // ...
     cv::dilate(detection,detection,close_elem); // closing...
     cv::erode (detection,detection,close_elem); // ...
 
+    
+
     cv::findContours(detection,contours, hierarchy, 
 		     CV_RETR_TREE,CV_CHAIN_APPROX_SIMPLE,cv::Point(0, 0));
-
-    /// Get the moments
-    std::vector<cv::Moments> mu(contours.size() );
-    for( int i = 0; i < contours.size(); i++ )
-      { mu[i] = moments( contours[i], false ); }
-
-    ///  Get the mass centers:
-    std::vector<cv::Point2f> mc( contours.size() );
-    for( int i = 0; i < contours.size(); i++ )
-      mc[i] = cv::Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 );
     
-    for( int i = 0; i < contours.size(); i++ )
-      auto res = pantiltzoom(mc[i].x, mc[i].y, u0; v0, pan, tilt, zoom); //corriger cette ligne
+    std::vector<cv::Moments> mu(contours.size());
+    std::vector<cv::Point2f> mc(contours.size());
+    
+
+    for( int j = 0; j < contours.size(); j++ ){
+      mu[j] = moments( contours[j], false );                             // Get the moments
+      mc[j] = cv::Point2f( mu[j].m10/mu[j].m00 , mu[j].m01/mu[j].m00 );  // Get the mass centers
+      auto res = pantiltzoom(mc[j].x, mc[j].y, u0, v0, pan, tilt, zoom); // &&corriger cette ligne&&
+      tl_turtle_track::PanTilt pantilt;
+      pantilt.pan = res.first;
+      pantilt.tilt = res.second;
+      vec_pantilt.push_back(pantilt);
+      circle(rgb, mc[j], 20, cv::Scalar(255,0,0));
+    }
+    cv::imshow( "hsv", hsv );  // Show our image inside it.
+    cv::imshow( "rgb", rgb );  // Show our image inside it.
+    cv::imshow( "detection", detection );  // Show our image inside it.
+    cv::imshow( "tresh", tresh );  // Show our image inside it.
+    cv::waitKey(0);
   }
+  pantilts.PanTilt_Array = vec_pantilt;
+  pub.publish(pantilts);
+}
+//::ConstPtr& msg
+void pose_callback(const tl_turtle_track::Axis::ConstPtr &msg,
+		   double &pan, double &tilt, double &zoom)
+{
   
+  pan  = msg->pan;
+  tilt = msg->tilt;
+  zoom = msg->zoom;
 }
 
 int main(int argc, char * argv[]) {
   
   ros::init(argc, argv, "axis_move");
+  double pan, tilt, zoom;
   ros::NodeHandle nh;
   ros::Publisher pub_pose_bot = nh.advertise<tl_turtle_track::PanTilts>("/pose_bot", 1);
 
-  ros::Subscriber sub_pos = nh.subscribe("/pose_in", 1000, pose_callback);
-  double pan, tilt, zoom;
+  ros::Subscriber sub_pos = nh.subscribe<tl_turtle_track::Axis>
+    ("pose_in", 1000, std::bind(pose_callback, std::placeholders::_1, std::ref(pan), std::ref(tilt),std::ref(zoom)));
+  
   image_transport::ImageTransport it(nh);
-  image_transport::Subscriber sub_img = it.subscribe("/image_in", 1, 
-						     bind(img_callback, _1, std::ref(pub_poe_bot),
-						       std::ref(pan), std::ref(tilt),std::ref(zoom)));
-  
-  //auto callback = [&u, &v, &pan, &tilt, &zoom]
-  
-  //ros::Subscriber sub_img = n2.subscibe("img_cam",1000,imgCallBack);					    
-  //ros::Subscriber sub_pos_cam = n3.subscibe("pos_cam",1000; imgCallBack);
+  image_transport::Subscriber sub_img = it.subscribe
+    ("image_in", 1, std::bind(img_callback, std::placeholders::_1, std::ref(pub_pose_bot), std::ref(pan), std::ref(tilt),std::ref(zoom)));
 
+  ros::spin();
+
+  
   /*
   ros::Rate poll_rate(100);
   while(pub.getNumSubscribers() == 0)
     poll_rate.sleep();
   std::cout << "I got one connection" << std::endl;
-
-  std::list<std::pair<double, double> > positions = { {0,0}, 
-						      {180, 0},
-						      {225, 0},
-						      {135, 0},
-						      {180, 0},
-						      {180, -90},
-						      {180, 10},
-						      {180, 0}};
+  std::list<std::pair<double, double> > positions = { {0,0},    {180, 0},
+						      {225, 0}, {135, 0},
+						      {180, 0}, {180, -90},
+						      {180, 10},{180, 0}};
 
   change_zoom(pub, 0);
   
